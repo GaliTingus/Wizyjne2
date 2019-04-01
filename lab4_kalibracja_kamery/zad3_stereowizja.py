6ed866510624b2162d9a102129d24fb462fe75c0
Daniel Hyjek <daniel.hyjek@gmail.com>
2019-04-01 11:11:02 +0200

    Modyfikacja parametrow

--- lab4_kalibracja_kamery/zad3_stereowizja.py
+++ lab4_kalibracja_kamery/zad3_stereowizja.py
@@ -27,14 +27,16 @@ def write_ply(fn, verts, colors):
 
 def main():
     print('loading images...')
-    imgL = cv.imread(cv.samples.findFile('aloes/aloeL.jpg'))  # downscale images for faster processing
-    imgR = cv.imread(cv.samples.findFile('aloes/aloeR.jpg'))
+    imgL = cv.pyrDown(cv.imread(cv.samples.findFile('aloes/aloeL.jpg')))  # downscale images for faster processing
+    imgR = cv.pyrDown(cv.imread(cv.samples.findFile('aloes/aloeR.jpg')))
+    # imgL = cv.imread(cv.samples.findFile('aloes/aloeL.jpg'))  # downscale images for faster processing
+    # imgR = cv.imread(cv.samples.findFile('aloes/aloeR.jpg'))
 
     # disparity range is tuned for 'aloe' image pair
-    window_size = 3
+    window_size = 5
     min_disp = 16
     num_disp = 112 - min_disp
-    stereo = cv.StereoSGBM_create(minDisparity=min_disp,
+    stereo = cv.StereoBM_create(minDisparity=min_disp,
                                   numDisparities=num_disp,
                                   blockSize=16,
                                   P1=8 * 3 * window_size ** 2,
@@ -48,25 +50,25 @@ def main():
     print('computing disparity...')
     disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
     cv.imshow("disp", np.uint8(disp))
-    print('generating 3d point cloud...', )
-    h, w = imgL.shape[:2]
-    f = 0.8 * w  # guess for focal length
-    Q = np.float32([[1, 0, 0, -0.5 * w],
-                    [0, -1, 0, 0.5 * h],  # turn points 180 deg around x-axis,
-                    [0, 0, 0, -f],  # so that y-axis looks up
-                    [0, 0, 1, 0]])
-    points = cv.reprojectImageTo3D(disp, Q)
-    colors = cv.cvtColor(imgL, cv.COLOR_BGR2RGB)
-    mask = disp > disp.min()
-    out_points = points[mask]
-    out_colors = colors[mask]
-    out_fn = 'out.ply'
-    write_ply('out.ply', out_points, out_colors)
-    print('%s saved' % 'out.ply')
-
-    cv.imshow('left', imgL)
-    cv.imshow('right', imgR)
-    cv.imshow('disparity', np.uint8((disp - min_disp) / num_disp))
+    # print('generating 3d point cloud...', )
+    # h, w = imgL.shape[:2]
+    # f = 0.8 * w  # guess for focal length
+    # Q = np.float32([[1, 0, 0, -0.5 * w],
+    #                 [0, -1, 0, 0.5 * h],  # turn points 180 deg around x-axis,
+    #                 [0, 0, 0, -f],  # so that y-axis looks up
+    #                 [0, 0, 1, 0]])
+    # points = cv.reprojectImageTo3D(disp, Q)
+    # colors = cv.cvtColor(imgL, cv.COLOR_BGR2RGB)
+    # mask = disp > disp.min()
+    # out_points = points[mask]
+    # out_colors = colors[mask]
+    # out_fn = 'out.ply'
+    # write_ply('out.ply', out_points, out_colors)
+    # print('%s saved' % 'out.ply')
+    #
+    # cv.imshow('left', imgL)
+    # cv.imshow('right', imgR)
+    # cv.imshow('disparity', np.uint8((disp - min_disp) / num_disp))
     cv.waitKey()
 
     print('Done')
